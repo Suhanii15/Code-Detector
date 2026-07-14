ce9141b1e7771f72abae127ac37ceb663104b4f7
Suhanii15 <suhanikabra931@gmail.com>
2026-07-14 10:28:52 +0530

    fixed frontend api integration for local host

--- server/server.js
+++ server/server.js
@@ -5,14 +5,12 @@ const mongoose = require('mongoose')
 const analyzeRoutes = require('./routes/analyze')
 
 const app = express()
+app.use(cors({
+  origin: 'http://localhost:5173',
+  credentials: true
+}));
 
 
-app.use(
-  cors({
-    origin: process.env.CLIENT_URL,
-    credentials: true,
-  })
-);
 app.use(express.json())
 
 mongoose.connect(process.env.MONGODB_URI)
